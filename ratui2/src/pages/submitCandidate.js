5066a606cd1e1a27547c6a4aec92ba8dcdc03455
Argeliosk8 <argeli.baca@gmail.com>
2024-03-10 11:44:59 -0600

    added sub_date to the new candidate submission

--- ratui2/src/pages/submitCandidate.js
+++ ratui2/src/pages/submitCandidate.js
@@ -4,7 +4,11 @@ import React, { useState } from "react";
 export const SubmitCandidate = ()=>{
     const uri = process.env.REACT_APP_URI
     const [file, setFile] = useState(null);
-    console.log(uri)
+    const [first_name, setFirstName] = useState(null)
+    const [last_name, setLastName] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [phone, setPhone] = useState(null)
+
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
     }
@@ -16,11 +20,35 @@ export const SubmitCandidate = ()=>{
         formData.append('resume', file)
 
         try {
-            const response = await fetch(uri + '/candidate/upload', {
+            const response1 = await fetch(uri + '/candidate/upload', {
                 method: 'POST',
                 body: formData
             })
-            console.log(response)
+
+            const data1 = await response1.json()
+            const publicUrl = await data1.url
+            console.log(publicUrl)
+
+
+            const newCandidate = {
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                phone: phone,
+                resume: publicUrl,
+                sub_date: new Date().toLocaleString()
+              }
+
+            console.log(newCandidate)
+            const response2 = await fetch(uri + '/candidate/submit', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newCandidate)
+            })
+
+            const data2 = await response2.json()
+            console.log(data2)
+
         } catch (error) {
             console.error("Error uploading file: ", error)
             
@@ -30,8 +58,16 @@ export const SubmitCandidate = ()=>{
     return(
         <div class="container-fluid">
             <form onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFileChange} />
-                <button type="submit">Upload</button>
+                <input type="file" onChange={handleFileChange} /> <br></br>
+                <label>First Name</label>
+                <input type="text" onChange={(e)=>{setFirstName(e.target.value)}} /><br></br>
+                <label>Last Name</label>
+                <input type="text" onChange={(e)=>{setLastName(e.target.value)}} /><br></br>
+                <label>Email</label>
+                <input type="text" onChange={(e)=>{setEmail(e.target.value)}} /><br></br>
+                <label>Phone</label>
+                <input type="text" onChange={(e)=>{setPhone(e.target.value)}} /><br></br>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
