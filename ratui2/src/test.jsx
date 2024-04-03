c7a9eeb887187be85eefbbd9e8e5c499d0b66418
Argeliosk8 <argeli.baca@gmail.com>
2024-04-03 12:56:43 -0600

    removed the exposed token

--- ratui2/src/test.jsx
+++ ratui2/src/test.jsx
@@ -5,7 +5,6 @@ const newProject = {
     "collaborators": []
 }
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTdmMzYzYTc1YmZjZmQ4ZDkzMGM0ZjUiLCJlbWFpbCI6InJvZHJpZ29AZ21haWwuY29tIiwicHdkIjoiJDJiJDEwJEpQci9TZTVkWUpnREh6d3Q2dVpLci5acUZNTkVyNVV0Y0JrRlppLlNvSmI0RDlLbXYwYnNLIiwic3RhdHVzIjoiQWN0aXZlIiwicHJvZmlsZSI6eyJmaXJzdF9uYW1lIjoiUm9kcmlnbyIsImxhc3RfbmFtZSI6IkdhdGljYSIsInJvbGUiOiJNYW5hZ2VyIiwicmVxcyI6W10sInJlcG9ydHMiOltdLCJjYW5kaWRhdGVzIjpbIjY1ODA3ZWNmZWRjZmJjMDY3ZWZhNTk0NiIsIjY1ODA4MzJjNGNkZmNkZDYyZWM3NzQwMCIsIjY1ODA4NzBlYWNiNmM0ODg5M2JhNTVhMiJdfSwiaWF0IjoxNzEyMTY2MDEzfQ.Vqo24yKlJ81ddm5CNFbNAbAuJlLn1dNYCiqry9fxEu0"
 const createProject = async (newProject) => {
     try {
         const resp = await fetch(`http://localhost:80/project/addone`, {
